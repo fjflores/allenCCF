c7bb22ef5cac164d831f9b9539d4e1cd854b0412
Francisco J. Flores <fjflores@mit.edu>
2022-11-10 17:08:38 -0500

    moves getallenpath from EA_in_vivo to this repo.

---
+++ getallenpath.m
@@ -0,0 +1,23 @@
+function ccfPath = getallenpath()
+% throw the root directory depending on the name of the computer
+pc = getenv( 'computername' );
+switch pc
+    case 'AJAX'
+        ccfPath = 'D:\Dropbox (MIT)\Protocols\Histology\AllenCCF';
+        
+    case 'ACHILLES'
+        ccfPath = 'E:\Dropbox (MIT)\Protocols\Histology\AllenCCF';
+        
+    case 'ZENZEN'
+        ccfPath = 'C:\Users\Isabella\Dropbox (Personal)\Protocols\Histology\AllenCCF';
+        
+    case 'BBU-PC'
+        ccfPath = 'D:\Dropbox (Personal)\Protocols\Histology\AllenCCF';
+        
+    case 'HADES'
+        ccfPath = 'D:\Dropbox (MIT)\Protocols\Histology\AllenCCF';
+        
+    otherwise
+        error( 'Unknown Computer' )
+        
+end
